c0b37b7d5ffaf56ed6ae55668ce59c957474567e
NowyTeam <jaj2232ajn@gmail.com>
2024-09-09 16:48:13 +0600

    Whj

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,26 @@
+import Style from "./Header.module.scss";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <h1>Hello world</h1>
-  )
+    <header className={Style.Header}>
+      <div className={Style.Header_Wrap}>
+        <div className={Style.Logo}>
+          <h1>
+            N<span>w</span>
+          </h1>
+        </div>
+        <div className={Style.Links}>
+          <ul>
+            <Link className={Style.link} href="/">
+              Home
+            </Link>
+            <Link className={Style.link} href="/">
+              Blog
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
